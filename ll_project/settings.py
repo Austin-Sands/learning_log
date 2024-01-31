d1c9681040d736de5d1047c17dc5ba82c2d3e401
Austin-Sands <austin_sands@comcast.net>
2024-01-30 22:00:32 -0500

    added user authentication and implemented users owning their topics. Can only see topics you own and must sign in to use the learning log

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # MY SETTINGS
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
 
 # Application definition
 
